46a13c244a2273c9825805492ca6cb112755079e
Graziela <grazielamilitaov@gmail.com>
2018-05-04 15:04:27 -0400

    Mapeamento do banco de dados

---
+++ AulaRevisão/src/br/edu/ifro/modelo/Aluno.java
@@ -0,0 +1,32 @@
+package br.edu.ifro.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno {
+    @Id
+    private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String nome;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    
+}
